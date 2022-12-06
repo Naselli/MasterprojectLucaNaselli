c109267c78f3cbaba854daa45f5a823e5d32a032
Luca Naselli <contact.lucanaselli@gmail.com>
2022-12-06 17:14:45 +0100

    finished grid and cube generator

--- Assets/Code/Scripts/Grid.cs
+++ Assets/Code/Scripts/Grid.cs
@@ -11,6 +11,8 @@ public class Grid : MonoBehaviour
 
     private Vector3[] _vertices;
     private Mesh _mesh;
+    private MeshFilter _meshFilter;
+
 
     private void Awake()
     {
@@ -19,13 +21,39 @@ public class Grid : MonoBehaviour
 
     private IEnumerator Generate()
     {
-        WaitForSeconds wait = new WaitForSeconds(.05f);
+        WaitForSeconds wait = new WaitForSeconds(0.05f);
+        GetComponent<MeshFilter>().mesh = _mesh = new Mesh();
+        _mesh.name = "Procedural Grid";
+
         _vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        Vector2[] uv = new Vector2[_vertices.Length];
+        Vector4[] tangents = new Vector4[_vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
         for (int i = 0, y = 0; y <= ySize; y++)
         {
             for (int x = 0; x <= xSize; x++, i++)
             {
                 _vertices[i] = new Vector3(x, y);
+                uv[i] = new Vector2((float) x / xSize, (float) y / ySize);
+                tangents[i] = tangent;
+                yield return wait;
+            }
+        }
+
+        _mesh.vertices = _vertices;
+        _mesh.uv = uv;
+        _mesh.tangents = tangents;
+
+        int[] triangles = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+                _mesh.triangles = triangles;
                 yield return wait;
             }
         }
@@ -33,15 +61,15 @@ public class Grid : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        if (_vertices == null)       
+        if (_vertices == null)
         {
             return;
         }
-        
+
         Gizmos.color = UnityEngine.Color.black;
         foreach (var t in _vertices)
         {
-            Gizmos.DrawSphere(t, .1f);
+            Gizmos.DrawSphere(t, .05f);
         }
     }
 }
